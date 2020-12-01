6f871596fd058c4207ff30b75f015e9d0d06bb68
seungcheol <astartes22@gmail.com>
2020-12-01 10:19:25 +0900

    edit mainpage

--- src/routes/mainPage.js
+++ src/routes/mainPage.js
@@ -1,5 +1,19 @@
-function MainPage() {
-  return <div>Hello, I'm main!</div>;
-}
+import FindYourWines from '../components/mainPage/findYourWines';
+import Explanation from '../components/mainPage/explanation';
+import Recommended from '../components/mainPage/recommended';
+import Flavor from '../components/filteringPage/flavor';
+import Pairings from '../components/filteringPage/pairings';
 
+function MainPage() {
+  return (
+    <>
+      {' '}
+      <FindYourWines>
+        {' '}
+        <Recommended /> <Pairings /> <Flavor />{' '}
+      </FindYourWines>{' '}
+      <Explanation />{' '}
+    </>
+  );
+}
 export default MainPage;
