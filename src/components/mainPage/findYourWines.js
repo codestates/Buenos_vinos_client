3de19a5d5d9228cd4d599b9a5a0759b77344541f
GriGni <amistadlee10@gmail.com>
2020-12-02 16:37:12 +0900

    make findYourWines and charousel

--- src/components/mainPage/findYourWines.js
+++ src/components/mainPage/findYourWines.js
@@ -1,8 +1,51 @@
-const FindYourWines = () => {
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import coverImage from '../../image/1.jpg';
+const FindYourWines = ({ children }) => {
   return (
     <>
-      <div>find your wines part</div>
-      <div>{children}</div>
+      <div>{children[0]}</div>
+      <div>
+        <React.Fragment>
+          <CssBaseline />
+          <Container maxWidth="lg" style={{ position: 'relative' }}>
+            <Typography
+              component="div"
+              style={{
+                backgroundImage: `url(${coverImage})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                height: '30vh',
+              }}
+            />
+            <Typography
+              component="div"
+              style={{
+                textAlign: 'center',
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                color: 'white',
+                transform: 'translate( -50%, -50% )',
+              }}
+            >
+              <Typography
+                style={{
+                  fontSize: '60px',
+                  textShadow: '1px 1px 2px black',
+                }}
+              >
+                Discover <br /> the Right Wine
+              </Typography>
+            </Typography>
+          </Container>
+        </React.Fragment>
+      </div>
+      <div>{children[2]}</div>
+      <div>{children[4]}</div>
     </>
   );
 };
