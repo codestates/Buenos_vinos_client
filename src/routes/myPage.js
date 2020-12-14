c79d6900da5b50b04bb9b66fe6beb42293a28546
seungcheol <astartes22@gmail.com>
2020-12-14 16:38:25 +0900

    restore

--- src/routes/myPage.js
+++ src/routes/myPage.js
@@ -1,25 +1,5 @@
-import { Button } from '@material-ui/core';
-import Axios from 'axios';
-import Cookies from 'js-cookie';
-
 function MyPage() {
-  const handleClick = () => {
-    console.log(Cookies.get('authorization'));
-    console.log(Cookies.get('userId'));
-    Axios({
-      method: 'get',
-      url: 'http://54.180.150.63:3000/user',
-    })
-      .then((res) => console.log(res))
-      .catch((err) => console.error(err));
-  };
-
-  return (
-    <div>
-      Hello, I'm MyPage!
-      <Button onClick={handleClick}>GET INFO TEST</Button>
-    </div>
-  );
+  return <div>Hello, I'm MyPage!</div>;
 }
 
 export default MyPage;
