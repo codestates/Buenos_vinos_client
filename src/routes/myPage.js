2c2d019319915eed5a216a2611fa75dd229afb8b
seungcheol <astartes22@gmail.com>
2020-12-14 15:17:10 +0900

    build test commit

--- src/routes/myPage.js
+++ src/routes/myPage.js
@@ -1,5 +1,25 @@
+import { Button } from '@material-ui/core';
+import Axios from 'axios';
+import Cookies from 'js-cookie';
+
 function MyPage() {
-  return <div>Hello, I'm MyPage!</div>;
+  const handleClick = () => {
+    console.log(Cookies.get('authorization'));
+    console.log(Cookies.get('userId'));
+    Axios({
+      method: 'get',
+      url: 'http://54.180.150.63:3000/user',
+    })
+      .then((res) => console.log(res))
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <div>
+      Hello, I'm MyPage!
+      <Button onClick={handleClick}>GET INFO TEST</Button>
+    </div>
+  );
 }
 
 export default MyPage;
